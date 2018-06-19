706bdb5db013cd99198241cc79ba8b3fad2860c7
Cayce Williams <caycew@umich.edu>
2018-06-19 13:20:08 -0400

    Main commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='google-actions',
+      version='1.0.0',
+      description='Client library for Actions on Google using python',
+      url='https://github.com/caycewilliams/actions-on-google-python',
+      author='Cayce Williams',
+      author_email='caycewilliams77@gmail.com',
+      license='MIT',
+      packages=['googleactions'],
+      zip_safe=False)
